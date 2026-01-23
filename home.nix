75dc758b3d5618bdb973d8a4828933f6de052e04
cm-kitagawa.zempei <kitagawa.zempei@classmethod.jp>
2026-01-23 13:17:58 +0900

    feat: nixfmtのパッケージ変更

--- home.nix
+++ home.nix
@@ -23,7 +23,7 @@
     # pkgs.hello
     pkgs.fzf-git-sh
     pkgs.nixd
-    pkgs.nixfmt-rfc-style
+    pkgs.nixfmt
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
