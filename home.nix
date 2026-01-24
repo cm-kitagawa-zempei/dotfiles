11d29313ceb64bf6f79a4f1b948ea1e040a185a8
cm-kitagawa.zempei <kitagawa.zempei@classmethod.jp>
2026-01-25 06:39:58 +0900

    feat: Helixの移行

--- home.nix
+++ home.nix
@@ -134,6 +134,152 @@
     };
   };
 
+  programs.helix = {
+    enable = true;
+
+    # LSPツールをPATHに追加
+    extraPackages = [
+      pkgs.pyright
+      pkgs.ruff
+    ];
+
+    # config.toml
+    settings = {
+      theme = "ayu_dark";
+      editor = {
+        line-number = "relative";
+        mouse = true;
+        scrolloff = 5;
+        shell = [
+          "zsh"
+          "-c"
+        ];
+        auto-save = true;
+        bufferline = "multiple";
+        color-modes = true;
+        end-of-line-diagnostics = "warning";
+        inline-diagnostics = {
+          cursor-line = "warning";
+          other-lines = "disable";
+        };
+        statusline = {
+          left = [
+            "mode"
+            "spinner"
+            "file-name"
+            "file-modification-indicator"
+          ];
+          center = [ "file-type" ];
+          right = [
+            "diagnostics"
+            "selections"
+            "position"
+            "file-encoding"
+          ];
+        };
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        file-picker.hidden = false;
+        search = {
+          smart-case = true;
+          wrap-around = true;
+        };
+        whitespace.render = "all";
+        indent-guides = {
+          render = true;
+          character = "╎";
+        };
+        soft-wrap.enable = false;
+        smart-tab = {
+          enable = true;
+        };
+        lsp.auto-signature-help = false;
+        lsp.display-messages = true;
+      };
+      keys = {
+        normal = {
+          esc = [
+            ":sh macism com.apple.keylayout.ABC"
+            "normal_mode"
+          ];
+          C-g = [
+            ":write-all"
+            ":new"
+            ":insert-output lazygit"
+            ":buffer-close!"
+            ":redraw"
+            ":reload-all"
+          ];
+          space.c = ":sh echo '@%{buffer_name}' | pbcopy";
+        };
+        select = {
+          space.c = ":sh echo '@%{buffer_name}:%{selection_line_start}-%{selection_line_end}' | pbcopy";
+        };
+        insert = {
+          esc = [
+            ":sh macism com.apple.keylayout.ABC"
+            "normal_mode"
+          ];
+        };
+      };
+    };
+    # languages.toml
+    languages = {
+      language = [
+        {
+          name = "python";
+          language-servers = [
+            "pyright"
+            "ruff"
+          ];
+          formatter = {
+            command = "ruff";
+            args = [
+              "format"
+              "-"
+            ];
+          };
+          auto-format = true;
+        }
+        {
+          name = "markdown";
+          soft-wrap.enable = true;
+        }
+        {
+          name = "nix";
+          language-servers = [ "nixd" ];
+          formatter = {
+            command = "nixfmt";
+          };
+          auto-format = true;
+        }
+      ];
+      language-server = {
+        pyright = {
+          command = "pyright-langserver";
+          args = [ "--stdio" ];
+        };
+        ruff = {
+          command = "ruff";
+          args = [
+            "server"
+            "--preview"
+          ];
+        };
+        nixd = {
+          command = "nixd";
+          args = [ "--semantic-tokens=true" ];
+          config.nixd = {
+            nixpkgs.expr = "import <nixpkgs> {}";
+          };
+        };
+      };
+    };
+  };
+
   programs.delta = {
     enable = true;
   };
